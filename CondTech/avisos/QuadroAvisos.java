8e9fce58745a1518ebc74a0b696cbb55108a0952
rbvfn profissional <rbvfn.profissional@gmail.com>
2025-04-09 12:29:22 -0300

    classe Usuário e Classe de avisos incomleta

--- CondTech/avisos/QuadroAvisos.java
+++ CondTech/avisos/QuadroAvisos.java
@@ -1,5 +1,12 @@
 package avisos;
+import java.util.UUID;
+import java.util.ArrayDeque;
+import comunicacaoInterna.Mensagem;
 
 public class QuadroAvisos {
+    private final String id = UUID.randomUUID().toString();
+    private String idSindico;
+    private final ArrayDeque<Mensagem> mensagens = new ArrayDeque<>();
+
     
 }
