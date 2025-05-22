79b6323554d35dbbc51ddc5193fa84bda469a7b1
rbvfn profissional <rbvfn.profissional@gmail.com>
2025-05-22 12:19:58 -0300

    refactory: adaptação da classe de repositório da entidade Usuario

--- CondTech/src/connection/Conexao.java
+++ CondTech/src/connection/Conexao.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Conection {
-    private static final String url = "jdbc:mysql://localhost:3306/testedb";
+public class Conexao {
+    private static final String url = "jdbc:mysql://localhost:3306/condtech_db";
     private static final String user = "root";
     private static final String password = "root";
     private static Connection connection;
--- CondTech/src/repository/UsuarioRepository.java
+++ CondTech/src/repository/UsuarioRepository.java
@@ -1,31 +1,84 @@
 package repository;
 import java.util.ArrayList;
 import java.util.List;
+import connection.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import model.Usuario;
 
 public class UsuarioRepository {
     
-    private final List<Usuario> usuarios;
+    private Connection cnn;
 
     public UsuarioRepository() {
-        this.usuarios = new ArrayList<>();
+        try {
+            this.cnn = Conexao.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void adicionarUsuario(Usuario usuario) {
-        usuarios.add(usuario);
+        String sql = "INSERT INTO usuarios (id, nome, cpf, email, senha, telefone, tipoUsuario, adm) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        try {
+            PreparedStatement ps = cnn.prepareStatement(sql);
+
+            ps.setString(1, usuario.getId());
+            ps.setString(2, usuario.getNome());
+            ps.setLong(3, usuario.getCpf());
+            ps.setString(4, usuario.getEmail());
+            ps.setString(5, usuario.getSenha());
+            ps.setLong(6, usuario.getTelefone());
+            ps.setString(7, usuario.getTipoUsuario());
+            ps.setBoolean(8, usuario.getAdmin());
+
+            ps.execute();
+            ps.close();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public Usuario buscarPorNome(String Nome) {
-        for (Usuario u : usuarios) {
-            if (u.getNome().equalsIgnoreCase(Nome)) {
-                return u;
-            }
+        String sql = "SELECT * FROM usuarios WHERE NOME = ?";
+        try {
+            PreparedStatement ps = cnn.prepareStatement(sql);
+            ps.setString(1, Nome);
+
+            ResultSet rs =  ps.executeQuery();
+            ps.close();
+
+            Usuario user = new Usuario(rs.getString("nome"), rs.getString("cpf"), rs.getString("email"), rs.getString("senha"), rs.getString("telefone"), rs.getString("tipoUsuario"), rs.getBoolean("adm"));
+            return user;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
         }
-        return null; //Usuario não foi encontrado
     }
 
     public List<Usuario> listarUsuarios() {
-        return new ArrayList<>(usuarios); // cópia da lista
+        String sql = "SELECT * FROM usuarios";
+        List<Usuario> usuarios = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = cnn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            ps.close();
+
+            while(rs.next()){
+                usuarios.add(new Usuario(rs.getString("nome"), rs.getString("cpf"), rs.getString("email"), rs.getString("senha"), rs.getString("telefone"), rs.getString("tipoUsuario"), rs.getBoolean("adm")));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return usuarios;
     }
 }
