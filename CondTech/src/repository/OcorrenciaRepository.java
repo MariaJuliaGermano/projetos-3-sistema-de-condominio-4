202c3555b21754807de1f06aa58dd99f67a6c839
rbvfn profissional <rbvfn.profissional@gmail.com>
2025-05-23 17:51:35 -0300

    refactory: adaptação da entidade Ocorrencia à persistencia de dados

--- CondTech/src/repository/OcorrenciaRepository.java
+++ CondTech/src/repository/OcorrenciaRepository.java
@@ -1,24 +1,67 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
+import connection.Conexao;
 import model.Ocorrencia;
 
 public class OcorrenciaRepository {
 
-    private final List<Ocorrencia> Ocorrencias;
+    private Connection cnn;
 
     public OcorrenciaRepository() {
-        this.Ocorrencias = new ArrayList<>();
+        try {
+            this.cnn = Conexao.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void adicionarOcorrencia(Ocorrencia Ocorrencia) {
-        Ocorrencias.add(Ocorrencia);
+    public void adicionarOcorrencia(Ocorrencia ocorrencia) {
+         String sql = "INSERT INTO ocorrencias (id, idMorador, dataRegistro, descricao, sttatus, anexos) VALUES(?, ?, ?, ?, ?, ?)";
+        
+        try {
+            PreparedStatement ps = cnn.prepareStatement(sql);
+
+            ps.setString(1, ocorrencia.getId());
+            ps.setString(2, ocorrencia.getIdMorador());
+            ps.setDate(3, java.sql.Date.valueOf(ocorrencia.getDataRegistro()));
+            ps.setString(4, ocorrencia.getDescricao());
+            ps.setString(5, ocorrencia.getStatus());
+            ps.setString(6, String.join(", ", ocorrencia.getAnexos()));
+
+            ps.execute();
+            ps.close();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<Ocorrencia> listarOcorrencias() {
-        return new ArrayList<>(Ocorrencias); // cópia da lista
+        String sql = "SELECT * FROM ocorrencias";
+        List<Ocorrencia> ocorrencias = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = cnn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                List<String> anexos = Arrays.asList(rs.getString("anexos"));
+                ocorrencias.add(new Ocorrencia(rs.getString("id"), rs.getString("idMorador"), rs.getDate("dataRegistro").toLocalDate(), rs.getString("descricao"), rs.getString("sttatus"), anexos));
+            }
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ocorrencias;
     }
 
 }
