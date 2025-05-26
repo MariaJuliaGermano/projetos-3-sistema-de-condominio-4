6b8cb1fa314c68f55a30a6339a4fb8f1efda3f8d
rbvfn profissional <rbvfn.profissional@gmail.com>
2025-05-26 16:44:29 -0300

    importação dos arquivos Jar do JUnit + testes para ChatController e LoginController

---
+++ CondTech/src/test/testController/TesteControllerLogin.java
@@ -0,0 +1,34 @@
+package test.testController;
+
+import repository.UsuarioRepository;
+import service.AuthService;
+import model.Usuario;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import controller.LoginController;
+
+public class TesteControllerLogin {
+    private final UsuarioRepository ur = new UsuarioRepository();
+    private final AuthService ar = new AuthService(ur);
+    private final LoginController lc = new LoginController(ar, ur);
+
+    @Test
+    public void loginUsuarioExistente(){
+        Usuario teste = lc.fazerLogin("admin", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918");
+
+        assertNotNull(teste);
+        assertEquals("admin", teste.getNome());
+        assertEquals("8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", teste.getSenha());
+    }
+
+    @Test
+    public void loginUsuarioInexistente(){
+        Usuario teste = lc.fazerLogin("teste", "teste");
+        assertNull(teste);
+    }
+}
