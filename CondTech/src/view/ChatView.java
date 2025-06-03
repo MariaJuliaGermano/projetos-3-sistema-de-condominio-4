9ec2f234975970c065fa681fad15d9b560716a4c
rbvfn profissional <rbvfn.profissional@gmail.com>
2025-06-03 16:29:16 -0300

    feature: clear no terminal para registro de ocorrencias + update: ajuste no menu do chat

--- CondTech/src/view/ChatView.java
+++ CondTech/src/view/ChatView.java
@@ -120,6 +120,16 @@ public class ChatView extends ChatViewAbstract{
         }
         
         chatController.enviarMensagemGrupo(usuarioLogado.getId(), conteudo);
+        try {
+            if(System.getProperty("os.name").contains("Windows")){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
         System.out.println("Mensagem enviada com sucesso!");
     }
 
